d787e5c6c4deff81cc216f34dadccfb12020f5e8
Soheeb <soheebamin@hotmail.com>
2021-01-05 09:38:38 -0500

    make lil program go from O(n) to 0(1):

--- main.py
+++ main.py
@@ -6,15 +6,7 @@ number_to_make = int(input("Number to make: "))
 if 0 > number or number > number_to_make:
     print("You can't select that number")
 else:
-    complement = None
-    i = 0
-    found_complement = False
-    while not found_complement:
-        if i + number == number_to_make:
-            complement = i
-            break
-        else:
-            i += 1
+    complement = -number + number_to_make
 print(f"The number to add to make {number_to_make} is {complement}")
 
 
